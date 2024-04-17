470128df90e575cbf53232b66d4ca3b64494adf4
MaxBlumenfeld7 <mblumenf@villanova.edu>
2024-04-16 21:20:19 -0400

    created files and did steps 1-3

---
+++ HW4JP/CurrencyConverterTest.java
@@ -0,0 +1,19 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CurrencyConverterTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testConvert() {
+		CurrencyConverter converter = new CurrencyConverter();
+		fail("Not yet implemented");
+		
+	}
+
+}
